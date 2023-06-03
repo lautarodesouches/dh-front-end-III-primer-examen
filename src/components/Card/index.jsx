7dd72ea14e4f863c56977bb66d4e2d748e769e38
Lautaro Desouches <lautarodesouches@outlook.com>
2023-06-02 22:02:01 -0300

    Card style

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,10 +1,15 @@
+import style from './style.module.css'
+
 export default function Card({ nombre = 'usuario', color = '#e1e1e1' }) {
     return (
-        <div>
-            <h2>Hola {nombre}!</h2>
-            <h3>Sabemos que tu color favorito es:</h3>
-            <div>
-                <p>{color}</p>
+        <div className={style.card}>
+            <h2 className={style.text}>Hola {nombre}!</h2>
+            <h3 className={style.text}>Sabemos que tu color favorito es:</h3>
+            <div
+                className={style.color}
+                style={{ backgroundColor: `#${color}` }}
+            >
+                <p className={style.colorText}>{color}</p>
             </div>
         </div>
     )
